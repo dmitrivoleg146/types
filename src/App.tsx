0d4f6e1cf5466c7273584a279ec90eefe2257bb0
Oleh <dmitrivoleg146@gmail.com>
2023-01-19 22:53:48 +0200

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
 import Person, { Cars, Country } from "./Person"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Provider} from "react-redux"
+import {store} from "./store"
+
 
 
 function App() {
-  const getAge =(name:string): number=>{
-   return 99;
-  }
+  // const getAge =(name:string): number=>{
+  //  return 99;
+  // }
   return (
     <div className="App">
-      <Person
+      {/* // redux toolkit is very similar to context */}
+      {/* <Person
       name="Oleh"
       email="dmitrivoleg146@gmail.com"
       age={21}
@@ -17,7 +22,16 @@ function App() {
       friends={["jake","lion","wolf"]}
       country={Country.Monaco}
       cars={Cars.Lamborgini}
-      />
+      /> */}
+       <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/login'element={<Login/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+            </Routes>
+          </Router>
+        </Provider>
     </div>
   );
 }
